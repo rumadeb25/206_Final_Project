b24ccd835b37daf50b221ec827a6f7678d1dfb35
Ruma Deb <rumad@umich.edu>
2020-04-14 12:52:13 -0400

    new

--- census.py
+++ census.py
@@ -8,47 +8,41 @@ import requests
 import re
 import csv
 
-#base census url 
+# base url for census 
+
 base_url = 
 
-conn = sqlite3.connect('geodata.sqlite')
+conn = sqlite3.connect('')
 cur = conn.cursor()
 
-# def setUpDatabase(db_name):
-#     path = os.path.dirname(os.path.abspath(__file__))
-#     conn = sqlite3.connect(path+'/'+db_name)
-#     cur = conn.cursor()
-#     return cur, conn
-
-cur.execute('''
-CREATE TABLE IF NOT EXISTS Locations (address TEXT, geodata TEXT)''')
+def setUpDatabase( ):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+ 'census.db')
+    cur = conn.cursor()
+    return cur, conn
 
 
-#reads from data and adds data to the database
-
-fh = open("where.data")
+cur.execute("CREATE TABLE IF NOT EXISTS Locations (address TEXT, data TEXT)")
 
 
+# reads from data and adds data to the database
+
+fh = open("  .data")
 
 
 
-#getting data from the web 
-#creating an api request with the request url 
-parms = dict()
-parms["address"] = address
-url =  base_url + parms
+def create_request_url( ):
 
-r = requests.get(url)
+# getting data from the web 
+# creating an api request with the request url 
 
-try:
-    dict = json.loads(r.text)
-except:
-    print("error when reading from url")  
-    continue
+    parms = 
+    url =  base_url + parms
+    r = requests.get(url)
 
+    try:
+        dict = json.loads(r.text)
+    except:
+        print("error when reading from url")  
+        dict = {}
 
-# print('Retrieving', url)
-# uh = urllib.request.urlopen(url, context=ctx)
-# data = uh.read().decode()
-# print('Retrieved', len(data), 'characters', data[:20].replace('\n', ' '))
-# count = count + 1 
