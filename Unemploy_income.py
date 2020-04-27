c3a699daf1ac701f562e5807c8e38de9bbea8ece
bhusain1 <60361128+bhusain1@users.noreply.github.com>
2020-04-27 00:33:28 -0400

    Add files via upload

--- Unemploy_income.py
+++ Unemploy_income.py
@@ -17,11 +17,13 @@ file_obj = open(full_path, "r")
 r = csv.reader(file_obj)
 counties_list = []
 income_list = []
+unemploy_list = []
 counties = []
 income = []
+umemploy = []
 
 cur.execute("DROP TABLE IF EXISTS MedIncome")
-cur.execute("CREATE TABLE IF NOT EXISTS MedIncome (county_num INTEGER, County TEXT, Median_Income INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS MedIncome (county_num INTEGER, County TEXT, Median_Income INTEGER, Unemployment INTEGER)")
 topnum = len(cur.execute("SELECT * FROM MedIncome").fetchall())
 bottomnum = topnum + 20
 
@@ -30,6 +32,8 @@ for i in r:
     counties_list = counties[6:]
     income.append(i[54])
     income_list = income[6:]
+    umemploy.append(i[52])
+    unemploy_list = umemploy[6:]
 
 file_obj.close()
 
@@ -43,7 +47,7 @@ for i in counties_list[topnum:bottomnum]:
 
     #print(income_list[j])
    
-    cur.execute("INSERT INTO MedIncome (county_num, County, Median_Income) VALUES (?,?,?)",(county_num, i, income_list[j]))
+    cur.execute("INSERT INTO MedIncome (county_num, County, Median_Income, Unemployment) VALUES (?,?,?,?)",(county_num, i, income_list[j], unemploy_list[j]))
     
     x += 1 
     y += 1
